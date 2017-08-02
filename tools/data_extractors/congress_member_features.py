d9f7379123e22fbfe8d8a0330e5c34dfa51233b5
Di-Wei Huang <dwh@cs.umd.edu>
2017-08-02 00:34:21 -0700

    output features for each congress member as a csv file; need fixing

---
+++ tools/data_extractors/congress_member_features.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import json
+
+class CongressMemberFeatures:
+    def __init__(self, fname_vote_history, fname_bill_ids, output_path):
+        self.congress_members = self.read_vote_history(fname_vote_history)
+        self.bill_ids = self.read_bill_ids(fname_bill_ids)
+        self.output_path = output_path
+        #self.bill_indices = {}
+        #for i, bill_id in enumerate(self.bill_ids):
+        #    self.bill_indices[bill_id] = i
+
+
+    def read_vote_history(self, fname):
+        with open(fname, 'r') as fp:
+            raw = json.load(fp)
+        return raw
+
+
+    def read_bill_ids(self, fname):
+        with open(fname, 'r') as fp:
+            raw = json.load(fp)
+        return raw
+
+
+    def gen(self):
+        samples = []
+        for m in self.congress_members:
+            sample = [m['id']]
+            for bill_id in self.bill_ids:
+                if bill_id in m['votes']:
+                    sample.append(m['votes'][bill_id])
+                else:
+                    sample.append(0)
+            samples.append(sample)
+        self.sanity_check(samples)
+
+        with open(self.output_path + '/' + self.__class__.__name__ + '.output.csv', 'w') as fp:
+            for sample in samples:
+                print(','.join([str(s) for s in sample]), file=fp)
+
+
+    def sanity_check(self, samples):
+        # check if there are single-valued rows
+        for sample in samples:
+            single_valued = True
+            val = sample[1]
+            for feature in sample[2:]:
+                if feature != val:
+                    single_valued = False
+                    break
+            if single_valued:
+                print(sample[0] + " has single-valued features")
+        # check if there are single-valued columns
+        cols = samples[0][1:]
+        single_valued = [True] * len(cols)
+        for sample in samples[1:]:
+            for i, feature in enumerate(sample[1:]):
+                if feature != cols[i]:
+                    single_valued[i] = False
+        single_valued_col_count = 0
+        for i, sv in enumerate(single_valued):
+            if sv:
+                single_valued_col_count += 1
+                print("feature {} has a single value only".format(i))
+        print("There are {}/{} single-valued columns".format(single_valued_col_count, len(cols)))
+        # TODO There are 550/675 single-valued columns, meaning there are 125 voted bills
+
+
+def main(argv):
+    if len(argv) < 4:
+        print("Usage: {} vote_extractor_output bill_finder_output output_path".format(argv[0]))
+        print()
+        print("Example: {} out/VoteExtractor.output.json out/BillFinder.output.json out".format(argv[0]))
+        return
+
+    CongressMemberFeatures(argv[1], argv[2], argv[3]).gen()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ tools/data_extractors/run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+if [ ! -f out/VoteExtractor.output.json ]; then
+    python vote_extractor.py ../../data out
+fi
+if [ ! -f out/BillFinder.output.json ]; then
+    python bill_finder.py ../../data out bill Taiwan China
+fi
+if [ ! -f out/CongressMemberFeatures.output.csv ]; then
+    python congress_member_features.py out/VoteExtractor.output.json \
+        out/BillFinder.output.json out
+fi
