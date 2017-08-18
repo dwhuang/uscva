820b4cf412a71c68febe97ffcdd39585bbd00d0b
Owen Chu <owenchu@gmail.com>
2017-08-17 23:26:26 -0700

    Allow selecting cell by clicking on text.
    This fixes #9.

--- web/src/app.js
+++ web/src/app.js
@@ -13,9 +13,21 @@ function FindHexagonVertices() {
   ];
 }
 
-function GetFillColor(d) {
+function GetDefaultCellBackgroundColor(d) {
   const value = d.umatrix_value * 255;
   return d3.rgb(value, value, value);
+}
+
+function GetSelectedCellBackgroundColor() {
+  return d3.rgb(66, 98, 244)
+}
+
+function GetDefaultCellTextColor() {
+  return d3.rgb(150, 150, 150);
+}
+
+function GetSelectedCellTextColor() {
+  return d3.rgb(255, 255, 255);
 }
 
 function GetCanvasSize() {
@@ -28,49 +40,71 @@ function GetCanvasSize() {
   return [x, y];
 }
 
+function OnCellClick() {
+  var selectedNode = d3.select(this.parentNode);
+  var previousSelectedNode = d3.select('#SelectedNode');
+  const isUnselectingNode = (selectedNode.attr('id') === 'SelectedNode');
+
+  if (!previousSelectedNode.empty()) {
+    previousSelectedNode.attr('id', null);
+    previousSelectedNode.select('polygon')
+      .attr('fill', GetDefaultCellBackgroundColor);
+    previousSelectedNode.select('text')
+      .attr('fill', GetDefaultCellTextColor);
+  }
+
+  if (isUnselectingNode) {
+    return;
+  }
+
+  selectedNode.attr('id', 'SelectedNode');
+  selectedNode.select('polygon')
+    .attr('fill', GetSelectedCellBackgroundColor);
+  selectedNode.select('text')
+    .attr('fill', GetSelectedCellTextColor);
+}
+
+function RenderGraph(entries) {
+  var canvasSize = GetCanvasSize();
+  var canvas = d3.select('#canvas')
+      .attr('width', canvasSize[0])
+      .attr('height', canvasSize[1])
+    .append('g')
+      .attr(
+        'transform',
+        'translate(' + [canvasSize[0]/2, canvasSize[1]/2] + ')'
+      )
+    .append('g')
+      .attr('transform', 'scale(12,-12)');
+  var cell = canvas.selectAll('g')
+      .data(entries)
+    .enter().append('g')
+      .attr('transform', function(entry) {
+        return 'translate(' + entry.centroid + ')';
+      });
+  cell.append('polygon')
+      .attr('points', FindHexagonVertices)
+      .attr('fill', GetDefaultCellBackgroundColor)
+      .on('click', OnCellClick);
+  cell.append('text')
+      .text(function(entry) {
+        switch (entry.labels.length) {
+          case 0: return null;
+          case 1: return '•';
+        }
+        return entry.labels.length;
+      })
+      .attr('fill', GetDefaultCellTextColor)
+      .attr('font-size', 1)
+      .attr('transform', 'scale(1,-1)')
+      .attr('text-anchor', 'middle')
+      .attr('alignment-baseline', 'middle')
+      .on('click', OnCellClick);
+}
+
 function Main() {
   d3.json('sbills-export.json', function(entries) {
-    var canvasSize = GetCanvasSize();
-    var canvas = d3.select('#canvas')
-        .attr('width', canvasSize[0])
-        .attr('height', canvasSize[1])
-      .append('g')
-        .attr(
-          'transform',
-          'translate(' + [canvasSize[0]/2, canvasSize[1]/2] + ')'
-        )
-      .append('g')
-        .attr('transform', 'scale(12,-12)');
-    var cell = canvas.selectAll('g')
-        .data(entries)
-      .enter().append('g')
-        .attr('transform', function(entry) {
-          return 'translate(' + entry.centroid + ')';
-        });
-    cell.append('polygon')
-        .attr('points', FindHexagonVertices)
-        .attr('fill', GetFillColor)
-        .on('click', function() {
-          d3.select('#SelectedHexagon')
-            .attr('id', null)
-            .attr('fill', GetFillColor);
-          d3.select(this)
-            .attr('id', 'SelectedHexagon')
-            .attr('fill', d3.rgb(66, 98, 244));
-        });
-    cell.append('text')
-        .text(function(entry) {
-          switch (entry.labels.length) {
-            case 0: return null;
-            case 1: return '•';
-          }
-          return entry.labels.length;
-        })
-        .attr('fill', d3.rgb(150, 150, 150))
-        .attr('font-size', 1)
-        .attr('transform', 'scale(1,-1)')
-        .attr('text-anchor', 'middle')
-        .attr('alignment-baseline', 'middle');
+    RenderGraph(entries);
   });
 }
 
