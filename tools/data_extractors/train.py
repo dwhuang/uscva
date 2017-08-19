922d9cf502489bb6f97c53f1971e6a7398383918
Di-Wei Huang <dwh@cs.umd.edu>
2017-08-18 22:02:45 -0700

    nit

--- tools/data_extractors/train.py
+++ tools/data_extractors/train.py
@@ -106,7 +106,6 @@ def main():
         labeled_map = som.label(labels, inputs)
         features = dict(zip(labels, inputs))
         profiles = CongressMemberProfile(config)
-        weights = som.weights
         # export for each node
         results = []
         for i, (centroid, vertices) in enumerate(
